2d4274b95e1d59b7f6e0e7c45ac489a2801c279c
wardy30 <hairul.wardy@digitalent.id>
2022-10-20 08:25:40 +0800

    adding favicon

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useRouter } from "next/router";
+import { onAuthStateChanged } from "firebase/auth";
+import Head from "next/head";
 
 import { getUserInfo } from "../src/utils/functions";
 import { AuthContext } from "../src/context/AuthProvider";
+import { auth } from "../src/config/firebase";
 
 const Home = () => {
   const { userAuthenticated } = useContext(AuthContext);
   const router = useRouter();
+  const [user, setUser] = useState();
 
   useEffect(() => {
     const userParsed = JSON.parse(getUserInfo());
@@ -16,9 +20,25 @@ const Home = () => {
     router.push("/login");
   }, []);
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      console.log(user);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <Head>
+        <title>
+          Welcome to koka shop | The Store that could be your got your dream
+          procuts
+        </title>
+      </Head>
+      <h1>
+        Welcome back{" "}
+        {user?.displayName !== null ? user?.displayName : user?.email}
+      </h1>
     </div>
   );
 };
