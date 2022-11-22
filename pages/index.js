bbd4973ea51444275ca026555edd820aa9786c9a
arialghifari <arialghifari99@gmail.com>
2022-11-23 00:05:55 +0800

    Landing page - hero section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Logo from "../src/components/Logo";
 
 const Home = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div className="landing container">
+      <div className="landing__header py-4 d-flex align-items-center justify-content-between">
+        <Logo />
+        <div className="landing__header__nav d-flex gap-2">
+          <a href="#">Home</a>
+          <a href="#">Blog</a>
+          <a href="#">Guide</a>
+          <a href="#">About</a>
+          <a href="#" class="btnLogin">
+            Login
+          </a>
+        </div>
+      </div>
+
+      <div className="landing__hero text-center">
+        <p className="landing__hero__title">
+          The habit tracker you get into the habit of using
+        </p>
+        <p className="landing__hero__subtitle">
+          Prilaku let's you simply track your habits.
+        </p>
+        <a href="#" className="landing__hero__cta">Go to the app</a>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
