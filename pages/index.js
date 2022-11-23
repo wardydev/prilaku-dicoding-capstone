b6348018fb492ed11e19a377943cc0a5167d941a
arialghifari <arialghifari99@gmail.com>
2022-11-24 00:38:18 +0800

    Landing page - feature section

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import Logo from "../src/components/Logo";
 
@@ -11,9 +12,9 @@ const Home = () => {
           <a href="#">Blog</a>
           <a href="#">Guide</a>
           <a href="#">About</a>
-          <a href="#" class="btnLogin">
-            Login
-          </a>
+          <Link href="/login">
+            <a className="btn-login">Login</a>
+          </Link>
         </div>
       </div>
 
@@ -24,7 +25,42 @@ const Home = () => {
         <p className="landing__hero__subtitle">
           Prilaku let's you simply track your habits.
         </p>
-        <a href="#" className="landing__hero__cta">Go to the app</a>
+        <Link href="/home">
+          <a className="landing__hero__cta">Go to the app</a>
+        </Link>
+      </div>
+
+      <div className="landing__image">
+        <div>
+          <img src="images/dashboard.png" alt="" />
+        </div>
+      </div>
+
+      <div className="landing__feature">
+        <p className="title">Track your own habit</p>
+        <p className="subtitle">Designed for daily use</p>
+
+        <div className="row gap-3">
+          <div className="landing__feature__card col-12 col-sm">
+            <img src="/images/ic_book.svg" alt="Icon book" />
+            <p className="title">Review your habits daily</p>
+            <p className="body">See your habits for the day</p>
+          </div>
+
+          <div className="landing__feature__card col-12 col-sm">
+            <img src="/images/ic_calendar.svg" alt="Icon calendar" />
+            <p className="title">Track your habit</p>
+            <p className="body">Easily track history of your habit</p>
+          </div>
+
+          <div className="landing__feature__card col-12 col-sm">
+            <img src="/images/ic_chart.svg" alt="" />
+            <p className="title">Habit summary</p>
+            <p className="body">
+              We give you statistics so you know where you going
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
