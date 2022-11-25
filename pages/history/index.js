54bec66ee941c3e8e5c0c8c06c342bdfa168c388
arialghifari <arialghifari99@gmail.com>
2022-11-25 16:19:16 +0800

    fix: language-2

--- pages/history/index.js
+++ pages/history/index.js
@@ -68,7 +68,7 @@ const History = () => {
       </div>
       <div className="row">
         <div className="col-8">
-          <Heading title="Habbit Finished" />
+          <Heading title="Habit Finished" />
           {habbits?.length === 0 ? (
             <h1>Tidak ada habbit finished hari ini</h1>
           ) : (
