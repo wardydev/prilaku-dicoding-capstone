5ae20810876f07ae40be7686ea464b173c536d93
arialghifari <arialghifari99@gmail.com>
2022-12-06 01:58:17 +0800

    fix: change prilaku jargon

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = () => {
 
       <div className="landing__hero text-center">
         <p className="landing__hero__title">
-          The habit tracker you get into the habit of using
+          Managing and tracking habit made easy just for you
         </p>
         <p className="landing__hero__subtitle">
           Prilaku let's you simply track your habits.
