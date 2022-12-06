a1d42622f547f3af49420bdf31952f054871c6bf
wardy30 <hairul.wardy@digitalent.id>
2022-12-06 08:06:52 +0800

    fix head placed

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-calendar/dist/Calendar.css";
 import "@hassanmojab/react-modern-calendar-datepicker/lib/DatePicker.css";
+import Head from "next/head";
 
 import "../styles/globals.css";
 import AuthProvider from "../src/context/AuthProvider";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <script
+          type="module"
+          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+        ></script>
+        <script
+          nomodule
+          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+        ></script>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Prilaku — Managing habits made easy</title>
+      </Head>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   );
 }
 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 
 import styles from "./Layout.module.scss";
 import Sidebar from "../Sidebar";
@@ -8,21 +7,6 @@ import Navbar from "../Navbar";
 const Layout = ({ children }) => {
   return (
     <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.png" />
-        <script
-          type="module"
-          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-        ></script>
-        <script
-          nomodule
-          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-        ></script>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Prilaku — Managing habits made easy</title>
-      </Head>
       <Sidebar />
       <Navbar />
       <div className={styles.childrenContainer}>{children}</div>
