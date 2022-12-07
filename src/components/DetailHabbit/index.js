48d5b7b4fdcacacf19898b180fe07c98a5381832
arialghifari <arialghifari99@gmail.com>
2022-12-07 22:08:34 +0800

    feat: detail-habit

--- src/components/DetailHabbit/index.js
+++ src/components/DetailHabbit/index.js
@@ -1,74 +1,73 @@
-import React, { useState } from "react";
-import { Calendar } from "@hassanmojab/react-modern-calendar-datepicker";
+import React from "react";
 
 import { deleteHabbit } from "../../utils/firebaseFunc";
-import { formatDate, formatterDateToObject } from "../../utils/functions";
+import { formatDate } from "../../utils/functions";
 import ButtonCustom from "../ButtonCustom";
-import Heading from "../Heading";
 import Modal from "../Modal";
+import styles from "./DetailHabbit.module.scss";
 
 const DetailHabbit = ({ setValue, dataDetailHabbit, setShowModal }) => {
-  const startDate = formatDate(new Date(dataDetailHabbit.data.startDate));
-  const endDate = formatDate(new Date(dataDetailHabbit.data.endDate));
-  const [selectedDayRange, setSelectedDayRange] = useState({
-    from: formatterDateToObject(new Date(dataDetailHabbit.data.startDate)),
-    to: formatterDateToObject(new Date(dataDetailHabbit.data.endDate)),
-  });
+  const startDate = formatDate(new Date(dataDetailHabbit.data.startDate)).split(
+    ","
+  )[1];
+  const endDate = formatDate(new Date(dataDetailHabbit.data.endDate)).split(
+    ","
+  )[1];
 
   return (
     <Modal setValue={setValue}>
-      <h3>{dataDetailHabbit?.data?.name}</h3>
-      <div className="row">
-        <div className="col-5">
-          <div className="my-3">
-            <Calendar
-              value={selectedDayRange}
-              onChange={() => null}
-              colorPrimary="#F58349"
-              colorPrimaryLight="#FBCEB6"
-              shouldHighlightWeekends
-            />
-          </div>
+      <div className={styles.wrapper}>
+        <div>
+          <p className="mb-1">HABIT NAME</p>
+          <p className={styles.data}>{dataDetailHabbit?.data?.name}</p>
         </div>
-      </div>
-      <div className="d-flex my-4">
-        <span>
-          Dari tanggal <strong className="text-warning">{startDate}</strong>{" "}
-        </span>{" "}
-        -
-        <span>
-          Sampai tanggal <strong className="text-warning">{endDate}</strong>
-        </span>
-      </div>
-      <div>
-        <Heading title="Note" />
-        <p className="fs-6 opacity-50">{dataDetailHabbit?.data?.note}</p>
-      </div>
-      <div className="mt-4 d-flex">
-        {dataDetailHabbit.data.isDone == false && (
-          <div className="me-3">
-            <ButtonCustom
-              title="Edit"
-              size="normal"
-              iconName="create"
-              isIcon={true}
-              handlePress={() => {
-                setShowModal(true);
-                setValue(false);
-              }}
-            />
-          </div>
-        )}
-        <ButtonCustom
-          title="Delete"
-          size="normal"
-          iconName="trash"
-          isIcon={true}
-          handlePress={() => {
-            setValue(false);
-            deleteHabbit("habbits", dataDetailHabbit.id);
-          }}
-        />
+
+        <div>
+          <p className="mt-3 mb-1">SCHEDULED</p>
+          <p className={styles.data}>
+            {startDate} {startDate != endDate && `- ${endDate}`}
+          </p>
+        </div>
+
+        <div>
+          <p className="mt-3 mb-1">AT TIME</p>
+          <p className={styles.data}>
+            <ion-icon name={dataDetailHabbit?.data?.time?.icon}></ion-icon>{" "}
+            {dataDetailHabbit?.data?.time?.name}
+          </p>
+        </div>
+
+        <div>
+          <p className="mt-3 mb-1">NOTE</p>
+          <p className={`${styles.data} ${styles.dataWide}`}>{dataDetailHabbit?.data?.note}</p>
+        </div>
+
+        <div className="mt-5 d-flex justify-content-center">
+          {dataDetailHabbit.data.isDone == false && (
+            <div className="me-3">
+              <ButtonCustom
+                title="Edit"
+                size="normal"
+                iconName="create"
+                isIcon={true}
+                handlePress={() => {
+                  setShowModal(true);
+                  setValue(false);
+                }}
+              />
+            </div>
+          )}
+          <ButtonCustom
+            title="Delete"
+            size="normal"
+            iconName="trash"
+            isIcon={true}
+            handlePress={() => {
+              setValue(false);
+              deleteHabbit("habbits", dataDetailHabbit.id);
+            }}
+          />
+        </div>
       </div>
     </Modal>
   );
