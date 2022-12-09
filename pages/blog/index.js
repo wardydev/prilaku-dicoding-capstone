19fb131d63255209eb8ee8df9f7fb695f75af936
arialghifari <arialghifari99@gmail.com>
2022-12-09 15:16:38 +0800

    feat: new articles

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -11,8 +11,8 @@ const Blog = () => {
 
   useEffect(() => {
     axios
-      .get("https://prekuel.com/wp-json/wp/v2/posts/?per_page=5")
-      .then((res) => setBlogs(res.data));
+      .get("http://prekuel.com/wp-json/wp/v2/posts/?category=248")
+      .then((res) => setBlogs(res.data.slice(0, 3)));
   }, []);
 
   return (
