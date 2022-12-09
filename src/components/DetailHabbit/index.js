5fc53fb3876a66327cef2bf0f2ee8658d20bff89
sukronsabari <skrnsabary27@gmail.com>
2022-12-09 20:38:31 +0800

    Starting Fix Detail Habbit

--- src/components/DetailHabbit/index.js
+++ src/components/DetailHabbit/index.js
@@ -6,6 +6,7 @@ import { formatDate, formatterDateToObject } from "../../utils/functions";
 import ButtonCustom from "../ButtonCustom";
 import Heading from "../Heading";
 import Modal from "../Modal";
+import styles from "./DetailHabbit.module.scss";
 
 const DetailHabbit = ({ setValue, dataDetailHabbit, setShowModal }) => {
   const startDate = formatDate(new Date(dataDetailHabbit.data.startDate));
@@ -17,34 +18,23 @@ const DetailHabbit = ({ setValue, dataDetailHabbit, setShowModal }) => {
 
   return (
     <Modal setValue={setValue}>
-      <h3>{dataDetailHabbit?.data?.name}</h3>
-      <div className="row">
-        <div className="col-5">
-          <div className="my-3">
-            <Calendar
-              value={selectedDayRange}
-              onChange={() => null}
-              colorPrimary="#F58349"
-              colorPrimaryLight="#FBCEB6"
-              shouldHighlightWeekends
-            />
-          </div>
+      <div className={styles["habit-detail"]}>
+        <h3>HABIT NAME</h3>
+        <div className={styles["habit-detail__name"]}>
+          <p>{dataDetailHabbit?.data?.name}</p>
+        </div>
+
+        <h3>DATE</h3>
+        <div className={styles["habit-detail__date"]}>
+          <p>{`${startDate} - ${endDate}`}</p>
+        </div>
+
+        <h3>NOTES</h3>
+        <div className={styles["habit-detail__notes"]}>
+          <p>{dataDetailHabbit?.data?.note}</p>
         </div>
       </div>
-      <div className="d-flex my-4">
-        <span>
-          Dari tanggal <strong className="text-warning">{startDate}</strong>{" "}
-        </span>{" "}
-        -
-        <span>
-          Sampai tanggal <strong className="text-warning">{endDate}</strong>
-        </span>
-      </div>
-      <div>
-        <Heading title="Note" />
-        <p className="fs-6 opacity-50">{dataDetailHabbit?.data?.note}</p>
-      </div>
-      <div className="mt-4 d-flex">
+{/*         
         {dataDetailHabbit.data.isDone == false && (
           <div className="me-3">
             <ButtonCustom
@@ -68,8 +58,7 @@ const DetailHabbit = ({ setValue, dataDetailHabbit, setShowModal }) => {
             setValue(false);
             deleteHabbit("habbits", dataDetailHabbit.id);
           }}
-        />
-      </div>
+        /> */}
     </Modal>
   );
 };
