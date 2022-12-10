31646150d9a46c12b47b105d113249556a6e3b3b
arialghifari <arialghifari99@gmail.com>
2022-12-10 11:38:23 +0800

    fix: for production

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className={styles["footer-content"]}>
           <div className={styles["footer-content__logo"]}>
             <Image
-              src="/images/logo2.svg"
+              src="/images/logo.png"
               width={48}
               height={48}
               alt="image profile"
@@ -23,22 +23,38 @@ const Footer = () => {
           <div className={styles["footer-content__social-links"]}>
             <ul>
               <li>
-                <a href="https://www.facebook.com/dicoding/" target="blank">
+                <a
+                  href="https://www.facebook.com/dicoding/"
+                  target="__blank"
+                  style={{ color: "#3c4043" }}
+                >
                   <ion-icon name="logo-facebook"></ion-icon>
                 </a>
               </li>
               <li>
-                <a href="https://twitter.com/dicoding" target="blank">
+                <a
+                  href="https://twitter.com/dicoding"
+                  target="__blank"
+                  style={{ color: "#3c4043" }}
+                >
                   <ion-icon name="logo-twitter"></ion-icon>
                 </a>
               </li>
               <li>
-                <a href="https://www.instagram.com/dicoding" target="blank">
+                <a
+                  href="https://www.instagram.com/dicoding"
+                  target="__blank"
+                  style={{ color: "#3c4043" }}
+                >
                   <ion-icon name="logo-instagram"></ion-icon>
                 </a>
               </li>
               <li>
-                <a href="https://github.com/wardydev/prilaku-dicoding-capstone" target="blank">
+                <a
+                  href="https://github.com/wardydev/prilaku-dicoding-capstone"
+                  target="__blank"
+                  style={{ color: "#3c4043" }}
+                >
                   <ion-icon name="logo-github"></ion-icon>
                 </a>
               </li>
@@ -69,7 +85,7 @@ const Footer = () => {
         <p>© 2022 Prilaku - Captstone Project Team C22 - 077</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,45 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const Navigation = ({ styles }) => {
+  const [hover, isHover] = useState(false);
+
   return (
-    <nav className={styles['navigation']}>
-      <ul className={styles['navigation-links']}>
+    <nav className={styles["navigation"]}>
+      <ul className={styles["navigation-links"]}>
         <li>
           <Link href="/home">
-              <a>
-                <ion-icon name="calendar-clear-outline"></ion-icon>
-                <span>habit</span>
-              </a>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "#3c4043",
+              }}
+            >
+              <ion-icon name="calendar-clear-outline"></ion-icon>
+              <span>habit</span>
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/history">
-              <a>
-                <ion-icon name="reload-circle-outline"></ion-icon>
-                <span>history</span>
-              </a>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "#3c4043",
+              }}
+            >
+              <ion-icon name="reload-circle-outline"></ion-icon>
+              <span>history</span>
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/article">
-              <a>
-                <ion-icon name="albums-outline"></ion-icon>
-                <span>article</span>
-              </a>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "#3c4043",
+              }}
+            >
+              <ion-icon name="albums-outline"></ion-icon>
+              <span>article</span>
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/users">
-              <a>
-                <ion-icon name="person-outline"></ion-icon>
-                <span>User</span>
-              </a>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "#3c4043",
+              }}
+            >
+              <ion-icon name="person-outline"></ion-icon>
+              <span>User</span>
+            </a>
           </Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navigation;
